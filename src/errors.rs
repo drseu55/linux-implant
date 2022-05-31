ced3dad367c0088565349613a14dd7236e0e3d5c
drseu55 <alpics1337@gmail.com>
2022-05-31 16:22:00 +0300

    feat: Add custom errors

--- src/errors.rs
+++ src/errors.rs
@@ -4,10 +4,40 @@ use thiserror::Error;
 pub enum ImplantError {
     #[error("Response error")]
     ResponseError,
+    #[error("Base64 decode error")]
+    DecodeError,
+    #[error("Arrayvec conversion error")]
+    ArrayvecError,
+    #[error("Bincode decode error")]
+    BincodeError,
 }
 
 impl std::convert::From<reqwest::Error> for ImplantError {
     fn from(_err: reqwest::Error) -> Self {
         ImplantError::ResponseError
+    }
+}
+
+impl std::convert::From<base64::DecodeError> for ImplantError {
+    fn from(_err: base64::DecodeError) -> Self {
+        ImplantError::DecodeError
+    }
+}
+
+impl std::convert::From<arrayvec::ArrayVec<u8, 32_usize>> for ImplantError {
+    fn from(_err: arrayvec::ArrayVec<u8, 32_usize>) -> Self {
+        ImplantError::ArrayvecError
+    }
+}
+
+impl std::convert::From<arrayvec::ArrayVec<u8, 24_usize>> for ImplantError {
+    fn from(_err: arrayvec::ArrayVec<u8, 24_usize>) -> Self {
+        ImplantError::ArrayvecError
+    }
+}
+
+impl std::convert::From<Box<bincode::ErrorKind>> for ImplantError {
+    fn from(_err: Box<bincode::ErrorKind>) -> Self {
+        ImplantError::BincodeError
     }
 }
